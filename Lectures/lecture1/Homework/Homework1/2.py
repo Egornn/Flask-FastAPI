a20d1a1fd270156bb890ee01df20442f74d4e2cd
Egor Nazdryuhin <egornnn@gmail.com>
2022-09-01 23:45:51 +0300

    updated

--- Lectures/lecture1/Homework/Homework1/2.py
+++ Lectures/lecture1/Homework/Homework1/2.py
@@ -1,17 +1,7 @@
-
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
 for i in range(8):
-    if i%2==0:
-        x=False
-    else:
-        x=True
-    if i%4//2==0:
-        y=False
-    else:
-        y=True
-    if i//4==0:
-        z=False
-    else:
-        z=True
-    print (f'For x={x}, y={y}, z=g{z} the required statement is {(not (x or y or z)) == (not x and not y and not z) }')
+    x = i % 2 == 0
+    y = i % 4 // 2 == 0
+    z = i // 4 == 0
+    print(f'For x={x}, y={y}, z=g{z} the required statement is {(not (x or y or z)) == (not x and not y and not z)}')
