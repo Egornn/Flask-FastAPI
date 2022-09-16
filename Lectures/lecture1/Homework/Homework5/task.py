c24d89e5e0e9cfd907f9cc462df9c26e6deb8b28
Egor Nazdryuhin <egornnn@gmail.com>
2022-09-16 03:47:46 +0300

    done tic-tac-toe

--- Lectures/lecture1/Homework/Homework5/task.py
+++ Lectures/lecture1/Homework/Homework5/task.py
@@ -1,5 +1,6 @@
 import random as r
 from PIL import Image, ImageDraw
+import os as os
 
 
 # 1/Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
@@ -79,9 +80,107 @@ def choose_mode(mode, candies, max_at_once):
         print('There is no such mode.')
 
 
-choose_mode(input('Use "pvp" or "pve" for 2 players and against computer mode respectively '), candy, max_candies
-# 3.Создайте программу для игры в ""Крестики-нолики"".
-#def draw_board (current_position):
+# choose_mode(input('Use "pvp" or "pve" for 2 players and against computer mode respectively '), candy, max_candies)
 
+# 3.Создайте программу для игры в ""Крестики-нолики"".
+board_path = 'tic-tac-toe.jpg'
+size = 300
+
+
+def coordinates_of_x(horizontal, vertical, left_or_right):
+    center_x = vertical * size // 3 + size // 6
+    center_y = horizontal * size // 3 + size // 6
+    offset = size // 12
+    if left_or_right == 'L':
+        return (center_x - offset, center_y - offset, center_x + offset, center_y + offset)
+    elif left_or_right == "R":
+        return (center_x + offset, center_y - offset, center_x - offset, center_y + offset)
+
+
+def coordinates_of_o(horizontal, vertical):
+    center_x = vertical * size // 3 + size // 6
+    center_y = horizontal * size // 3 + size // 6
+    offset = size // 8
+    return (center_x - offset, center_y - offset, center_x + offset, center_y + offset)
+
+
+def draw_x_o(horizontal, vertical, x_or_o):
+    im = Image.open(board_path)
+    draw = ImageDraw.Draw(im)
+    if x_or_o == 'X':
+        draw.line(coordinates_of_x(horizontal, vertical, "L"), fill=(255, 0, 0), width=10)
+        draw.line(coordinates_of_x(horizontal, vertical, "R"), fill=(255, 0, 0), width=10)
+    elif x_or_o == 'O':
+        draw.ellipse(coordinates_of_o(horizontal, vertical), fill=(255, 255, 255), outline=(0, 256, 0), width=10)
+    im.save(board_path, quality=100)
+
+
+def initial_setup(size):
+    try:
+        os.remove(board_path)
+    except:
+        pass
+    im = Image.new('RGB', (size, size), (256, 256, 256))
+    draw = ImageDraw.Draw(im)
+    for i in range(size // 3, size, size // 3):
+        draw.line((i, 0, i, size), fill=(0, 0, 0), width=10)
+        draw.line((0, i, size, i), fill=(0, 0, 0), width=10)
+    im.save(board_path, quality=100)
+    return [[' ' for x in range(3)] for y in range(3)]
+
+
+def check_if_win(board_position, symbol):
+    win = [symbol for x in range(3)]
+    return board_position[0] == win \
+           or board_position[1] == win \
+           or board_position[2] == win \
+           or [board_position[i][0] for i in range(3)] == win \
+           or [board_position[i][1] for i in range(3)] == win \
+           or [board_position[i][2] for i in range(3)] == win \
+           or [board_position[i][i] for i in range(3)] == win \
+           or [board_position[i][-i - 1] for i in range(3)] == win
+
+
+def get_input_coordinates(board, symbol):
+    is_done = False
+    coord = [-1, -1]
+    while (not (0 <= coord[0] <= 2)) or (not (0 <= coord[1] <= 2)) or not is_done:
+        coord[0] = int(input(f'Enter the row to draw a {symbol} from 1 to 3): ')) - 1
+        coord[1] = int(input(f'Enter the column to draw a {symbol} from 1 to 3): ')) - 1
+        try:
+            correct = board[coord[0]][coord[1]] == " "
+            if correct:
+                is_done = True
+            else:
+                print('This space is already taken')
+        except:
+            pass
+        print('')
+    return coord
+
+
+def play_cycle(board):
+    turn = 0
+    current_player = 1
+    corresponding_symbols = {1: 'X', 2: 'O'}
+    while not check_if_win(board, corresponding_symbols[current_player % 2 + 1]):
+        if turn == 9:
+            current_player = -1
+            break
+        coordinate = get_input_coordinates(board, corresponding_symbols[current_player])
+        board[coordinate[0]][coordinate[1]] = corresponding_symbols[current_player]
+        draw_x_o(coordinate[0], coordinate[1], corresponding_symbols[current_player])
+        current_player = current_player % 2 + 1
+        turn += 1
+    if current_player == 1:
+        print('Congratulations to O player')
+    elif current_player == 2:
+        print('Congratulations to the X player')
+    elif current_player == -1:
+        print("That's a tie")
+
+
+current_board = initial_setup(size)
+play_cycle(current_board)
 
 # 4.Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
