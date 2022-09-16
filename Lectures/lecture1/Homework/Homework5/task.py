dd7049832b69a10e5ca747b5ed6385eb7fc43540
Egor Nazdryuhin <egornnn@gmail.com>
2022-09-16 10:54:49 +0300

    size=900

--- Lectures/lecture1/Homework/Homework5/task.py
+++ Lectures/lecture1/Homework/Homework5/task.py
@@ -84,7 +84,7 @@ def choose_mode(mode, candies, max_at_once):
 
 # 3.Создайте программу для игры в ""Крестики-нолики"".
 board_path = 'tic-tac-toe.jpg'
-size = 1500
+size = 900
 
 
 def initial_setup(size):
